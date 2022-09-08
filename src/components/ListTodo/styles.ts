6bd589637a86f80f41f133facbdbfe96b58041eb
Miguel Machado <miiguellmp@gmail.com>
2022-09-08 20:00:15 -0300

    fix(list): break word

--- src/components/ListTodo/styles.ts
+++ src/components/ListTodo/styles.ts
@@ -18,6 +18,7 @@ export const ListItem = styled.li<ListItemProps>`
   border-bottom: 2px solid #484040;
   text-decoration: ${({ isDone }) => (isDone ? 'line-through' : 'none')};
   color: ${({ isDone }) => (isDone ? 'rgba(15, 144, 161, 0.57)' : '#fff')};
+  word-break: break-all;
 
   &:last-child {
     border-bottom: 0;
